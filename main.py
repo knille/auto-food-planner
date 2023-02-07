2371193a6763bb5a16739fcbecb7a48b3fbd07c0
Kristian <kristian.git@proton.me>
2023-02-07 22:28:48 +0100

    broke it

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import sys
 import sqlite3
 
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
     QApplication,
-    QMainWindow
+    QMainWindow,
+    QLabel,
+    QPushButton,
+    QTabWidget,
+    QWidget
 )
+
+from layout_colorwidget import Color
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Auto Food Planner")
+
+        tabs = QTabWidget()
+        tabs.setTabPosition(QTabWidget.TabPosition.West)
+        tabs.setMovable(False)
+
+        tabs.addTab()
+
+        # tabs.addTab(QTabWidget.setTabText(0, "Plan dinner"))
+        # tabs.addTab.setTabText(1, "Add recipe")
+        # tabs.addTab.setTabText(2, "All recipies")
+
+        self.setCentralWidget(tabs)
 
 app = QApplication(sys.argv)
 
